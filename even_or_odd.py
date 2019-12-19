7bb8adc3c87449cad1ed9f26148d1a191dd33a66
Moriah Sherman <moriahsherman@gmail.com>
2019-12-19 13:55:09 +0000

    adding exit to function

--- even_or_odd.py
+++ even_or_odd.py
@@ -1,14 +1,19 @@
 # a short script using mod to determin if number is even or odd
+import sys
 
 def even_or_odd():
-    print(" \nThis script can tell you if a number is even or odd.")
+    print(" \nThis script can tell you if a number is even or odd. (type STOP to exit)")
     usr_num = input("What number would you like to test?: ")
  
     try:
         float(usr_num)
     except ValueError:
-        print("You have entered {0} which is a not valid number for this function!".format(usr_num))
-        return even_or_odd()
+        if usr_num != "STOP":
+            print("You have entered {0} which is a not valid number for this function!".format(usr_num))
+            return even_or_odd()
+        else:
+            print("You have entered STOP, this python script will now exit. Goodbye!\n")
+            sys.exit()
 
     usr_num = float(usr_num)
     print("Congrats! You have entered {0} which is a valid number for this function!".format(usr_num))
